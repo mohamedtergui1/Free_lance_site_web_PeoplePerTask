d478b2b55992e57a6d3fd147a4a8e8a2c34ce887
elouissi <yassinelouissi67@gmail.com>
2023-11-01 22:10:16 +0100

    terminison du 20% du projet

---
+++ javascript/serch.js
@@ -0,0 +1,10 @@
+var btn = document.getElementById("btnlist")
+var list = document.getElementById("list")
+ 
+    btn.addEventListener("click",()=>{
+    
+    list.classList.toggle("display")
+
+})
+ 
+ 
