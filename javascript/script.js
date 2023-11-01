acdcddf1bfbb2470f306d838f1feb8c998b274e5
abdelghaniAit <aaittamghart8@gmail.com>
2023-11-01 11:08:33 +0100

    Add dev script command

--- javascript/script.js
+++ javascript/script.js
@@ -1,17 +1,17 @@
-$( document ).ready(function() {
-    if(window.innerWidth<1023){
+$(document).ready(function () {
+    if (window.innerWidth < 1023) {
         $("#navbar").hide(0);
     }
-    $("#hamberger_btn").click(function(){
-    $("#navbar").slideToggle(500);
-   })
-   console.log(document.with)
-   function test_nav_status(){
-        if(window.innerWidth>=1023){
+    $("#hamberger_btn").click(function () {
+        $("#navbar").slideToggle(500);
+    })
+    console.log(document.with)
+    function test_nav_status() {
+        if (window.innerWidth >= 1023) {
             $("#navbar").show(0);
         }
-       
-        setTimeout(test_nav_status,500)
-   }
-   test_nav_status();
+
+        setTimeout(test_nav_status, 500)
+    }
+    test_nav_status();
 });
