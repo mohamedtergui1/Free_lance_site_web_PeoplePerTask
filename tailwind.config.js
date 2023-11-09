89df478229e2d8fc64cf6c71bd3753fa2a1ffa52
nadaelmahfoudi <elmahfoudinada17@gmail.com>
2023-11-09 14:20:31 +0100

    adding modal

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,9 @@ module.exports = {
       colors: {
         'custom-green': '#00FF0A',
       },
+      backdropFilter: { 
+        'blur-gray': 'blur(5px) grayscale(0.5)',
+      },
     },
   },
   plugins: [],
